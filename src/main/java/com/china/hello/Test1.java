a1b31f3b066f4b5ce7c4c48b7946bd0820d58fe3
47888955 <47888955@qq.com>
2019-02-19 20:23:59 +0800

    test1.java

---
+++ src/main/java/com/china/hello/Test1.java
@@ -0,0 +1,28 @@
+/**   
+ * Copyright © 2019 eSunny Info. Tech Ltd. All rights reserved.
+ * 
+ * 功能描述：
+ * @Package: com.china.hello 
+ * @author: Administrator   
+ * @date: 2019年2月19日 下午8:23:36 
+ */
+package com.china.hello;
+
+/**   
+* Copyright: Copyright (c) 2019 LanRu-Caifu
+* 
+* @ClassName: Test1.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: Administrator
+* @date: 2019年2月19日 下午8:23:36 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2019年2月19日     Administrator           v1.0.0               修改原因
+*/
+public class Test1 {
+
+}
