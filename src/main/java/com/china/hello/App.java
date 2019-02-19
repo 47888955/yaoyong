8db2cc80306c222c0153e0e44a7a722a140313a4
47888955 <47888955@qq.com>
2019-02-19 16:53:34 +0800

    Update App.java

--- src/main/java/com/china/hello/App.java
+++ src/main/java/com/china/hello/App.java
@@ -8,8 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "a!1111" );
-        System.out.println( "btttt!" );
-        System.out.println( "cttt!" );
+        System.out.println( "111111111111a!1111" );
+        System.out.println( "1111111111111btttt!" );
+        System.out.println( "1111111111cttt!" );
+                System.out.println( "111111111111a!1111" );
+        System.out.println( "1111111111111btttt!" );
+        System.out.println( "1111111111cttt!" );
     }
 }
