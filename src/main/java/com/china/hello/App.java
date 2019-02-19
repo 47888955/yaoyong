98f2c27e2755ff46a90fc9e1d8917622b842322f
47888955 <47888955@qq.com>
2019-02-19 16:27:22 +0800

    Update App.java

--- src/main/java/com/china/hello/App.java
+++ src/main/java/com/china/hello/App.java
@@ -8,9 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "本地的啊!!" );
         System.out.println( "server!" );
-        System.out.println( "本地的啊!!" );
-        System.out.println( "server!" );
+
     }
 }
