6e0bc62d98ba77f9543372aa60b9521b7c4ec63a
47888955 <47888955@qq.com>
2019-02-19 16:59:16 +0800

    Update App.java

--- src/main/java/com/china/hello/App.java
+++ src/main/java/com/china/hello/App.java
@@ -9,13 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "a!11114" );
-        System.out.println( "btttt3!" );
-        System.out.println( "cttt!2" );
-        System.out.println( "111111111111a!1111" );
-        System.out.println( "1111111111111btttt!" );
-        System.out.println( "1111111111cttt!" );
-                System.out.println( "111111111111a!1111" );
-        System.out.println( "1111111111111btttt!" );
-        System.out.println( "1111111111ctt!" );
     }
 }
