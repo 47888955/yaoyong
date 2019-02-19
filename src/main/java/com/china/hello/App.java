5a0e583313a9618d1cf853b1a66f53372e4eacf2
47888955 <47888955@qq.com>
2019-02-19 16:56:37 +0800

    1

--- src/main/java/com/china/hello/App.java
+++ src/main/java/com/china/hello/App.java
@@ -11,5 +11,11 @@ public class App
         System.out.println( "a!11114" );
         System.out.println( "btttt3!" );
         System.out.println( "cttt!2" );
+        System.out.println( "111111111111a!1111" );
+        System.out.println( "1111111111111btttt!" );
+        System.out.println( "1111111111cttt!" );
+                System.out.println( "111111111111a!1111" );
+        System.out.println( "1111111111111btttt!" );
+        System.out.println( "1111111111cttt!" );
     }
 }
