0924dacf7b929ed0f9f4052c70f7df87517a0737
47888955 <47888955@qq.com>
2019-02-19 16:53:11 +0800

    1

--- src/main/java/com/china/hello/App.java
+++ src/main/java/com/china/hello/App.java
@@ -9,9 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "a!1111" );
-        System.out.println( "b!" );
-        System.out.println( "c!" );
-        System.out.println( "ttt!" );
         System.out.println( "btttt!" );
         System.out.println( "cttt!" );
     }
